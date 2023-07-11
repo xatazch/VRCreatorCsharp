5efbae491b5e6fdfe53979c241eee7c5042bd2d1
Pierre Fransson <pierre.fransson.ext@siemens-energy.com>
2023-07-11 16:16:38 +0200

    Added destroy balloon

--- Assets/Scripts/DartBehaviour.cs
+++ Assets/Scripts/DartBehaviour.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class DartBehaviour : MonoBehaviour
 {
     public float speed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 5.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
         gameObject.transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Balloon"))
+        {
+            Debug.Log("Hit Balloon");
+            Destroy(collision.gameObject);
+        }
     }
 }
